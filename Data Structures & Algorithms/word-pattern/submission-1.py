95a6bb9f2681fbf15856043124fa1087ae493386
Ben Bao <baodeakin2023@gmail.com>
2026-08-28 14:25:40 +1000

    Add: word-pattern - submission-1

---
+++ Data Structures & Algorithms/word-pattern/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        
+        # Every pattern character must have one corresponding word
+        if len(pattern) != len(words):
+            return False
+        
+        # Create 2 dictionaries
+        letter_to_word = {}
+        word_to_letter = {}
+
+        for letter, word in zip(pattern, words):
+            # The letter already maps to a different word
+            if letter in letter_to_word:
+                if letter_to_word[letter] != word:
+                    return False
+            
+            # The word already maps to a different letter
+            if word in word_to_letter:
+                if word_to_letter[word] != letter:
+                    return False
+            
+            # Create the mappings if they are new
+            letter_to_word[letter] = word
+            word_to_letter[word] = letter
+
+        return True
+
